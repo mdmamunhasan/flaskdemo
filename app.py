07ce9e32400ef4e2846ba5b24c5d48c4330b41bd
Mamun Hasan <mamunhasan@w3engineers.com>
2018-05-28 09:34:27 +0600

    flask debug

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development_key'
+
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+if __name__ == "__main__":
+    app.run()
